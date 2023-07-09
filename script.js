5e3d160f38278340543de26bc23cdd754ad1e652
Mayur borgude <72288591+Mayborg121@users.noreply.github.com>
2023-07-09 21:50:20 +0530

    First commit
    Success

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.navbar-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.navbar-menu').classList.toggle('active');
+  });
+  
